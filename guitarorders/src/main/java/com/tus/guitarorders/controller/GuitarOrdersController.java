b4cee4dc62dd2767ff2fd8740a756ff5ca8124e2
joeaoregan <joe.oregan@gmail.com>
2026-04-17 00:34:30 +0100

    lab10: SpringCloud config

--- guitarorders/src/main/java/com/tus/guitarorders/controller/GuitarOrdersController.java
+++ guitarorders/src/main/java/com/tus/guitarorders/controller/GuitarOrdersController.java
@@ -1,5 +1,8 @@
 package com.tus.guitarorders.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,58 +23,73 @@ import com.tus.guitarorders.service.IGuitarOrdersService;
 
 import jakarta.validation.Valid; // Lab 7
 import jakarta.validation.constraints.Pattern; // Lab 7
-import lombok.AllArgsConstructor;
+//import lombok.AllArgsConstructor; // Lab 10 commented out to implement constructor injection manually
 
 @RestController
-@RequestMapping(path = "/api/accounts", produces = MediaType.APPLICATION_JSON_VALUE)
-@AllArgsConstructor
+@RequestMapping(path = "/api/guitars", produces = MediaType.APPLICATION_JSON_VALUE)
+//@AllArgsConstructor // Lab 10 commented out to implement constructor injection manually
 @Validated
 public class GuitarOrdersController {
 
-    IGuitarOrdersService iGuitarOrdersService;
+	private IGuitarOrdersService iGuitarOrdersService;
 
-    @GetMapping("/sayHello")
-    public String sayHello() {
-        return "Hello World";
-    }
+	@Value("${build.version}")
+	private String buildVersion;
 
-    @GetMapping()
-    public ResponseEntity<CustomerDto> fetchOrderDetails(@RequestParam
-            @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits") String mobileNumber) { // Lab 7
-        CustomerDto customerDto = iGuitarOrdersService.fetchOrder(mobileNumber);
-        return ResponseEntity.status(HttpStatus.OK).body(customerDto);
-    }
+	// Lab 10 - Implement constructor injection for IGuitarOrdersService
+	public GuitarOrdersController(IGuitarOrdersService iGuitarOrdersService) {
+		this.iGuitarOrdersService = iGuitarOrdersService;
+	}
 
-    // Lab 3
-    @PostMapping()
-    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) { // Lab 7
-        iGuitarOrdersService.createOrder(customerDto);
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(new ResponseDto(GuitarOrdersConstants.STATUS_201, GuitarOrdersConstants.MESSAGE_201));
-    }
+	@GetMapping("/build-info")
+	public ResponseEntity<String> getBuildInfo() {
+		return ResponseEntity.status(HttpStatus.OK).body(buildVersion);
+	}
 
-    @PutMapping()
-    public ResponseEntity<ResponseDto> updateOrderDetails(@Valid @RequestBody CustomerDto customerDto) { // Lab 7
-        boolean isUpdated = iGuitarOrdersService.updateOrder(customerDto);
-        if (isUpdated) {
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(new ResponseDto(GuitarOrdersConstants.STATUS_200, GuitarOrdersConstants.MESSAGE_200));
-        } else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseDto(GuitarOrdersConstants.STATUS_500, GuitarOrdersConstants.MESSAGE_500));
-        }
-    }
+	@GetMapping("/sayHello")
+	public String sayHello() {
+		return "Hello World";
+	}
 
-    @DeleteMapping()
-    public ResponseEntity<ResponseDto> deleteOrderDetails(@RequestParam
-            @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits") String mobileNumber) { // Lab 7
-        boolean isDeleted = iGuitarOrdersService.deleteOrder(mobileNumber);
-        if (isDeleted) {
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(new ResponseDto(GuitarOrdersConstants.STATUS_200, GuitarOrdersConstants.MESSAGE_200));
-        } else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseDto(GuitarOrdersConstants.STATUS_500, GuitarOrdersConstants.MESSAGE_500));
-        }
-    }
+	@GetMapping()
+	public ResponseEntity<CustomerDto> fetchOrderDetails(
+			@RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits") String mobileNumber) { // Lab
+																																	// 7
+		CustomerDto customerDto = iGuitarOrdersService.fetchOrder(mobileNumber);
+		return ResponseEntity.status(HttpStatus.OK).body(customerDto);
+	}
+
+	// Lab 3
+	@PostMapping()
+	public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) { // Lab 7
+		iGuitarOrdersService.createOrder(customerDto);
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(new ResponseDto(GuitarOrdersConstants.STATUS_201, GuitarOrdersConstants.MESSAGE_201));
+	}
+
+	@PutMapping()
+	public ResponseEntity<ResponseDto> updateOrderDetails(@Valid @RequestBody CustomerDto customerDto) { // Lab 7
+		boolean isUpdated = iGuitarOrdersService.updateOrder(customerDto);
+		if (isUpdated) {
+			return ResponseEntity.status(HttpStatus.OK)
+					.body(new ResponseDto(GuitarOrdersConstants.STATUS_200, GuitarOrdersConstants.MESSAGE_200));
+		} else {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(new ResponseDto(GuitarOrdersConstants.STATUS_500, GuitarOrdersConstants.MESSAGE_500));
+		}
+	}
+
+	@DeleteMapping()
+	public ResponseEntity<ResponseDto> deleteOrderDetails(
+			@RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits") String mobileNumber) { // Lab
+																																	// 7
+		boolean isDeleted = iGuitarOrdersService.deleteOrder(mobileNumber);
+		if (isDeleted) {
+			return ResponseEntity.status(HttpStatus.OK)
+					.body(new ResponseDto(GuitarOrdersConstants.STATUS_200, GuitarOrdersConstants.MESSAGE_200));
+		} else {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(new ResponseDto(GuitarOrdersConstants.STATUS_500, GuitarOrdersConstants.MESSAGE_500));
+		}
+	}
 }
